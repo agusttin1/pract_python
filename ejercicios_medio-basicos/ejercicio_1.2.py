9e2d22084254a0618a7084edc621112400c88dcc
agusttin1 garcia <garciaagusttin@gmail.com>
2023-01-25 02:23:07 -0300

    fix ej_1.2  b)

--- ejercicios_medio-basicos/ejercicio_1.2.py
+++ ejercicios_medio-basicos/ejercicio_1.2.py
@@ -2,7 +2,40 @@
 
 # a) mini-quizz
 
+yes_or_no = input('Desea realizar la siguiente quiz? (si/no): ')
 
+def quiz(answer):
+    if answer.lower() == "si":
+        
+        name=input("primero ingrese su nombre: ")
+        print('------------------------------------------------')
+        print(f'------------Bienvenido/a {name}------------')
+        
+        score = 0
+       
+        question_1 = int(input("a) cuantos jugadores se encuentran en una cancha de futbol: "))
+        question_2= int(input("b) cuantos cambios se pueden hacer por partidos actualmente: "))
+        if question_1 == 11:
+            score = score + 1
+            print('a) Respuesta acertada')
+           
+        else:
+            print('a) respuesta incorrecta')
+            
+        if question_2 == 5:
+            score = score + 1
+            print('b) Respuesta acertada ')
+            
+        else:
+            print('b) respuesta incorrecta')
+    
+        print(f'su score total fue de  {score}')
+    else:
+        print(f'lo esperaremos en la proxima')
+        quit()
+    
+    
+quiz(yes_or_no)
 
 
 #b)
